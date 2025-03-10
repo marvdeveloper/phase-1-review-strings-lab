2b8346b57c1f24bbe8b09e6a17865a019052704a
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-03-10 09:16:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// 1. welcomeMessage should contain "Welcome to Flatbook, " followed by the value stored in currentUser
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 2. excitedWelcomeMessage should contain the uppercased value of welcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// 3. shortGreeting should contain "Welcome, " followed by the first initial of the value stored in currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
